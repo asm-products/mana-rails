caae8be5de435b7c1b5feabb4330f9291de2ecd5
aaronmallen <aaronmallen4@gmail.com>
2014-11-22 16:45:24 -0600

    * updated create session logic to allow for a user to pass name or email * updated verbiage in views * adjusted tests for new username logic
    resolve bounty #75

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -16,7 +16,7 @@ class ActiveSupport::TestCase
     password    = options[:password]    || 'password'
     remember_me = options[:remember_me] || '1'
     if integration_test?
-      post login_path, session: { email:       user.email,
+      post login_path, session: { username:    user.email,
                                   password:    password,
                                   remember_me: remember_me }
     else
