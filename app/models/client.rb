e14c40672ed41243b3cd366301037e9dc9535101
Andrew Salmons <salmons@ocas.ca>
2014-11-22 20:06:54 -0500

    Clients model streamlined

--- app/models/client.rb
+++ app/models/client.rb
@@ -3,26 +3,37 @@ class Client < ActiveRecord::Base
   
   has_many :contacts
   
-  validates :name, presence: true, length: { minimum: 4 }
-  validates :short_code, uniqueness: true, length: { minimum: 4, maximum: 6 }, allow_blank: true
+  validates :name,
+    presence: true,
+    length: { minimum: 4 }
+    
+  validates :short_code,
+    uniqueness: true, length: { minimum: 4, maximum: 6 },
+    allow_blank: true
+  
+  def short_code=(new_short_code)
+      super new_short_code.downcase.parameterize
+  end
+  
+  def unique_short_code=(new_short_code)
+      self.short_code = generate_unique_short_code(new_short_code)
+  end
   
   def default_values
-    self.short_code = self.short_code.present? ? self.short_code.downcase : self.name[0..3].downcase
-    self.short_code = self.short_code.parameterize
-    if !Client.where(short_code: self.short_code).take.nil?
-      self.short_code = generate_unique_short_code
+      if short_code.blank? && name.present?
+          self.unique_short_code = self.name[0..3]
     end
   end
   
   def generate_unique_short_code
     i = 0
-    new_short_code = self.short_code
-    while !Client.where(short_code: new_short_code).take.nil? do
-      i += 1
-      new_short_code = self.short_code + i.to_s
-      n = self.short_code[0..5].length - i.to_s.length
-      new_short_code = new_short_code.length <= 6 ? new_short_code : self.short_code[0..n] + i.to_s 
+    generated_short_code = new_short_code
+    
+    while Client.where(short_code: generated_short_code).exists? do
+        i += 1
+        generated_short_code = i.to_s.rjust(6, new_short_code)
     end
-    return new_short_code
+    
+    generated_short_code
   end
 end
