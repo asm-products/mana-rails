ab5882645d6704687ac010b7806342b76c42ad02
Sam Sheffres <ssheffres@gmail.com>
2014-11-25 17:04:02 +0300

    Fix Client Creation Spec

--- spec/controllers/clients_spec.rb
+++ spec/controllers/clients_spec.rb
@@ -5,15 +5,18 @@ describe ClientsController, :type => :controller do
     login
     @client = Client.make!
   end
-
+  
   it "#index is 200" do
     get :index
     expect(response).to have_http_status 200
   end
 
   it "it creates clients" do
-    client = Client.create
-    expect(response).to have_http_status 200
+    client = Client.make!(name: "TestName2", short_code: "12346")
+    post :create, client: {name: client.name}
+
+    expect(response).to_not render_template :new
+    expect(response).to render_template nil
   end
 
   it "destroys Clients" do
