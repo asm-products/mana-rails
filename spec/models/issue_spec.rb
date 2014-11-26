5e71b857641d8a476c90a793b70dc7b56ad6c60b
aaronmallen <aaronmallen4@gmail.com>
2014-11-26 01:03:45 -0600

    commented out issue spec pending sam's rewrite

--- spec/models/issue_spec.rb
+++ spec/models/issue_spec.rb
@@ -3,12 +3,12 @@ require 'rails_helper'
 describe Issue, :type => :model do
   before { @issue = Issue.make! }
 
-  it "is valid" do
-    expect @issue.valid?
-  end
+#  it "is valid" do
+#    expect @issue.valid?
+#  end
   
-  it "validates presence of subject" do
-    @issue.subject = nil
-    expect !@issue.valid?
-  end
+#  it "validates presence of subject" do
+#    @issue.subject = nil
+#    expect !@issue.valid?
+#  end
 end
