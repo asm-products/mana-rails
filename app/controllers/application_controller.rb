e85ac02088e11d17e112f29f1fa1f6a13da186c7
Philipp Wesner <pwesner@innomind.info>
2015-01-22 21:22:46 +0100

    removed all helper methods included in application controller in seperate concern

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,61 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  include TeamsHelper
-  include ApiHelper
-
-    # Logs in the given user.
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
-  # Remembers a user in a persistent session.
-  def remember(user)
-    user.remember
-    cookies.permanent.signed[:user_id] = user.id
-    cookies.permanent[:remember_token] = user.remember_token
-  end
-
-  # Returns the current logged-in user (if any).
-  def current_user
-    if (user_id = session[:user_id])
-      @current_user ||= User.find_by(id: user_id)
-    elsif (user_id = cookies.signed[:user_id])
-      user = User.find_by(id: user_id)
-      if user && user.authenticated?(cookies[:remember_token])
-        log_in user
-        @current_user = user
-      end
-    end
-  end
-  helper_method :current_user
-
-  # Redirects to login_page if not logged in.
-  def authenticate_user!
-    if current_user.nil?
-      redirect_to login_path
-    end
-  end
-
-  # Returns true if the user is logged in, false otherwise.
-  def logged_in?
-    !current_user.nil?
-  end
-  helper_method :logged_in?
-
-  # Forgets a persistent session.
-  def forget(user)
-    user.forget
-    cookies.delete(:user_id)
-    cookies.delete(:remember_token)
-  end
-
-  # Logs out the current user.
-  def log_out
-    session.delete(:user_id)
-    @current_user = nil
-  end
+  include Session
 
   # Handle Access Denied Exceptions
   rescue_from CanCan::AccessDenied do |exception|
