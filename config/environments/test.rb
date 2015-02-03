7fa300a9c26eb9910d8df536e2431e5de75e9232
Philipp Wesner <pwesner@innomind.info>
2015-02-03 22:03:59 +0100

    fixed spec

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -34,6 +34,7 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  config.log_level = :warn
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 end
