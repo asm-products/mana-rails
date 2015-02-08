aa33d2ee1b0b1c56bb8d3b7806a2c34c73103a5f
PILO <zurrfc1980@hotmail.com>
2015-02-08 11:01:34 +0100

    Added forms style

--- db/schema.rb
+++ db/schema.rb
@@ -16,9 +16,6 @@ ActiveRecord::Schema.define(version: 20150201182052) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "clients", force: true do |t|
     t.string   "name"
     t.text     "address"
