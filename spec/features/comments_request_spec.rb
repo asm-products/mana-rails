ebb31c1c0055625e1ceff46fc713d4d07e91cab1
PILO <zurrfc1980@hotmail.com>
2015-02-26 18:07:40 +0100

    Added Cients List view. Fixed :active sidebar link color.

--- spec/features/comments_request_spec.rb
+++ spec/features/comments_request_spec.rb
@@ -28,7 +28,7 @@ describe CommentsController do
 
     def create_client
       visit clients_path
-      click_link 'Create New Client'
+      click_link 'New Client'
       fill_in :client_name, with: "testname"
       fill_in :client_address, with: "testaddress"
       fill_in :client_phone, with: "123123"
