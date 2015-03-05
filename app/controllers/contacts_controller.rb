ef3ac83e251d5f937d2a734a3dfb8db9b000291e
PILO <zurrfc1980@hotmail.com>
2015-03-05 20:08:08 +0100

    Revert "# This is a combination of 2 commits."
    This reverts commit f7118467d3b7622b0b0700d519e877184ed70f69.

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -9,7 +9,7 @@ class ContactsController < ApplicationController
     @contact = @client.contacts.new
     @profile = Profile.new
   end
-
+  
   def create
     @client = Client.find_by(id: params[:client_id]) || Client.find_by(short_code: params[:client_id])
     @contact = @client.contacts.new(contact_params)
@@ -25,26 +25,26 @@ class ContactsController < ApplicationController
       render 'new'
     end
   end
-
+  
   def edit
     @profile = @contact.profile
   end
-
+  
   def update
     @profile = @contact.profile
     if @contact.profile.update(profile_params)
-      redirect_to client_path(@client.short_code, @contact.name)
+      redirect_to client_user_path(@client.short_code, @contact.name)
     else
       render 'edit'
     end
   end
-
+  
   def destroy
     # Don't actually destroy the user, remove them from the client
     @contact.update_attribute(:client_id, nil)
     redirect_to client_path(@client.short_code)
   end
-
+  
   def verify
     @allow_verify = true
     @client = Client.find_by(short_code: params[:client_id])
@@ -53,7 +53,7 @@ class ContactsController < ApplicationController
       @allow_verify = false
     end
   end
-
+  
   def verified
     @client = Client.find_by(short_code: params[:client_id])
     @contact = @client.contacts.find_by(special_key: params[:id])
@@ -78,18 +78,18 @@ class ContactsController < ApplicationController
   end
 
   private
-
+  
   def set_client_contact
     @client = Client.find_by(short_code: params[:client_id])
     @contacts = @client.contacts
     @contact = @contacts.find_by(id: params[:id]) || @contacts.find_by(name: params[:id])
   end
-
+  
   def contact_params
     params.require(:contact).permit(:name, :email, :password, :password_confirmation)
   end
-
+  
   def profile_params
-    params.require(:profile).permit(:first_name, :last_name, :job_title, :phone)
+    params.require(:user_profile).permit(:first_name, :last_name, :job_title, :phone)
   end
 end
